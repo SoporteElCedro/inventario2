36c026d318e6486475111f1818e0cae6f57688eb
SoporteElCedro <soportetecnico@tlelcedro.com>
2022-11-07 17:06:37 -0600

    Update product_out.php

--- vistas/product_out.php
+++ vistas/product_out.php
@@ -14,30 +14,7 @@
         $id = (isset($_GET['product_id'])) ? $_GET['product_id'] : 0;
         $id=limpiar_cadena($id);
         
-        $inicio = ($pagina>0) ? (($pagina * $registros)-$registros) : 0;
-	    $tabla="";
-
-	    $campos="producto.producto_id,producto.producto_codigo,producto.producto_nombre,producto.producto_precio,producto.producto_stock,producto.producto_foto,producto.categoria_id,producto.usuario_id,categoria.categoria_id,categoria.categoria_nombre,usuario.usuario_id,usuario.usuario_nombre,usuario.usuario_apellido";
-
-        if(isset($busqueda) && $busqueda!=""){
-
-            $consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id WHERE producto.producto_codigo LIKE '%$busqueda%' OR producto.producto_nombre LIKE '%$busqueda%' ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-            $consulta_total="SELECT COUNT(producto_id) FROM producto WHERE producto_codigo LIKE '%$busqueda%' OR producto_nombre LIKE '%$busqueda%'";
-
-        }elseif($categoria_id>0){
-
-            $consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id WHERE producto.categoria_id='$categoria_id' ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-            $consulta_total="SELECT COUNT(producto_id) FROM producto WHERE categoria_id='$categoria_id'";
-
-        }else{
-
-            $consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-            $consulta_total="SELECT COUNT(producto_id) FROM producto";
-
-        }
+        
 
         $conexion=conexion();
 
