aee07eb37643b9c83e5bca1e59e6342eee63ffb9
SoporteElCedro <soportetecnico@tlelcedro.com>
2022-11-10 18:02:54 -0600

    salida de producto

---
+++ DB/inventario(1).sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-11-2022 a las 23:22:00
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `categoria_id` int(7) NOT NULL,
+  `categoria_nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `categoria_ubicacion` varchar(150) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`categoria_id`, `categoria_nombre`, `categoria_ubicacion`) VALUES
+(1, 'Herramientas', 'Taller');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `producto_id` int(20) NOT NULL,
+  `producto_codigo` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
+  `producto_nombre` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
+  `producto_precio` decimal(30,2) NOT NULL,
+  `producto_stock` int(25) NOT NULL,
+  `producto_foto` varchar(500) COLLATE utf8_spanish2_ci NOT NULL,
+  `producto_fecha` timestamp NULL DEFAULT NULL,
+  `categoria_id` int(7) NOT NULL,
+  `usuario_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`producto_id`, `producto_codigo`, `producto_nombre`, `producto_precio`, `producto_stock`, `producto_foto`, `producto_fecha`, `categoria_id`, `usuario_id`) VALUES
+(2, 'SN', 'pinzas de corte', '100.00', 3, 'pinzas_de_corte_57.jpg', '2022-11-01 18:14:17', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salida_producto`
+--
+
+CREATE TABLE `salida_producto` (
+  `salida_id` int(11) NOT NULL,
+  `salida_cantidad` int(11) NOT NULL,
+  `salida_fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `salida_usuid` int(11) NOT NULL,
+  `salida_prodid` int(11) NOT NULL,
+  `salida_catid` int(11) NOT NULL,
+  `salida_personalid` int(11) NOT NULL,
+  `salida_unidadid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `usuario_id` int(10) NOT NULL,
+  `usuario_nombre` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `usuario_apellido` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `usuario_usuario` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `usuario_clave` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `usuario_email` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `usuario_fecha` timestamp NULL DEFAULT NULL,
+  `usuario_permiso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`usuario_id`, `usuario_nombre`, `usuario_apellido`, `usuario_usuario`, `usuario_clave`, `usuario_email`, `usuario_fecha`, `usuario_permiso`) VALUES
+(1, 'Administrador', 'Principal', 'Administrador', '$2y$10$wLH89oVX1ueS9HyllevpFekTY5dZ.ohCAJy8Ulloka5L5DhT6h6QC', '', '2022-10-31 17:09:20', 1),
+(2, 'Mario', 'Ramos', 'Mramos', '$2y$10$ovVOrJgmF/jexPDLyJ8idOISjv6SsIk7jYgsiHrBa9HzkHd1u3PTG', 'soportetecnico@tlelcedro.com', '2022-10-31 10:59:34', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`producto_id`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `salida_producto`
+--
+ALTER TABLE `salida_producto`
+  ADD PRIMARY KEY (`salida_id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usuario_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `categoria_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `producto_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `salida_producto`
+--
+ALTER TABLE `salida_producto`
+  MODIFY `salida_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `usuario_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`categoria_id`),
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- vistas/product_out.php
+++ vistas/product_out.php
@@ -1,115 +1,57 @@
-<div class="container is-fluid mb-6">
-    <h1 class="title">Prestamos</h1>
-    <h2 class="subtitle">Lista de productos a prestar</h2>
-</div>
-
 <div class="container pb-6 pt-6">
     <?php
-		// include "./inc/btn_back.php";
-        echo'<p class="has-text-right pt-4 pb-4">
-                <a href="#" class="button is-link is-rounded btn-back">Agregar Prestamo</a>
-            </p>';
-
-		require_once "./php/main.php";
-        $id = (isset($_GET['product_id'])) ? $_GET['product_id'] : 0;
-        $id=limpiar_cadena($id);
-        
-        $inicio = ($pagina>0) ? (($pagina * $registros)-$registros) : 0;
-	    $tabla="";
-
-	    $campos="producto.producto_id,producto.producto_codigo,producto.producto_nombre,producto.producto_precio,producto.producto_stock,producto.producto_foto,producto.categoria_id,producto.usuario_id,categoria.categoria_id,categoria.categoria_nombre,usuario.usuario_id,usuario.usuario_nombre,usuario.usuario_apellido";
-
-        if(isset($busqueda) && $busqueda!=""){
-
-            $consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id WHERE producto.producto_codigo LIKE '%$busqueda%' OR producto.producto_nombre LIKE '%$busqueda%' ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-            $consulta_total="SELECT COUNT(producto_id) FROM producto WHERE producto_codigo LIKE '%$busqueda%' OR producto_nombre LIKE '%$busqueda%'";
-
-        }elseif($categoria_id>0){
-
-            $consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id WHERE producto.categoria_id='$categoria_id' ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-            $consulta_total="SELECT COUNT(producto_id) FROM producto WHERE categoria_id='$categoria_id'";
-
-        }else{
-
-            $consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-            $consulta_total="SELECT COUNT(producto_id) FROM producto";
-
-        }
-
-        $conexion=conexion();
-
-        $datos = $conexion->query($consulta_datos);
-        $datos = $datos->fetchAll();
-
-        $total = $conexion->query($consulta_total);
-        $total = (int) $total->fetchColumn();
-
-        $Npaginas =ceil($total/$registros);
-
-        if($total>=1 && $pagina<=$Npaginas){
-            $contador=$inicio+1;
-            $pag_inicio=$inicio+1;
-            foreach($datos as $rows){
-                $tabla.='
-                    <article class="media">
-                        <figure class="media-left">
-                            <p class="image is-64x64">';
-                            if(is_file("./img/producto/".$rows['producto_foto'])){
-                                $tabla.='<img src="./img/producto/'.$rows['producto_foto'].'">';
-                            }else{
-                                $tabla.='<img src="./img/producto.png">';
-                            }
-                $tabla.='</p>
-                        </figure>
-                        <div class="media-content">
-                            <div class="content">
-                            <p>
-                                <strong>'.$contador.' - '.$rows['producto_nombre'].'</strong><br>
-                                <strong>CODIGO:</strong> '.$rows['producto_codigo'].', <strong>PRECIO:</strong> $'.$rows['producto_precio'].', <strong>STOCK:</strong> '.$rows['producto_stock'].', <strong>CATEGORIA:</strong> '.$rows['categoria_nombre'].', <strong>REGISTRADO POR:</strong> '.$rows['usuario_nombre'].' '.$rows['usuario_apellido'].'
-                            </p>
-                            </div>
-                            <div class="has-text-right">
-                            <a href="index.php?vista=product_out&product_id='.$rows['producto_id'].'" class="button is-link is-rounded is-small">Registrar Salida</a>
-                                <a href="index.php?vista=product_img&product_id_up='.$rows['producto_id'].'" class="button is-link is-rounded is-small">Imagen</a>
-                                <a href="index.php?vista=product_update&product_id_up='.$rows['producto_id'].'" class="button is-success is-rounded is-small">Actualizar</a>
-                                <a href="'.$url.$pagina.'&product_id_del='.$rows['producto_id'].'" class="button is-danger is-rounded is-small">Eliminar</a>
-                            </div>
-                        </div>
-                    </article>
-
-                    <hr>
-                ';
-                $contador++;
-            }
-            $pag_final=$contador-1;
-        }else{
-            if($total>=1){
-                $tabla.='
-                    <p class="has-text-centered" >
-                        <a href="'.$url.'1" class="button is-link is-rounded is-small mt-4 mb-4">
-                            Haga clic acá para recargar el listado
-                        </a>
-                    </p>
-                ';
-            }else{
-                $tabla.='
-                    <p class="has-text-centered" >No hay registros en el sistema</p>
-                ';
-            }
-        }
-
-        if($total>0 && $pagina<=$Npaginas){
-            $tabla.='<p class="has-text-right">Mostrando productos <strong>'.$pag_inicio.'</strong> al <strong>'.$pag_final.'</strong> de un <strong>total de '.$total.'</strong></p>';
-        }
-
-        $conexion=null;
-        echo $tabla;
-
-        if($total>=1 && $pagina<=$Npaginas){
-            echo paginador_tablas($pagina,$Npaginas,$url,7);
-        }
+        require_once "./php/main.php";
     ?>
+    <div class="columns">
+        <div class="column is-one-third">
+            <h2 class="title has-text-centered">PRESTAMO / SALIDAS</h2>
+            <form action="" method="POST" class="FormularioAjax" autocomplete="off">
+                <div>
+                        <?php
+                            $personal=conexion();
+                            $personal=$personal->query("SELECT peronal_id, personal_nombre, personal_apaterno FROM personal");
+                            
+                            if($personal->rowCount()>0){
+                                $personal=$personal->fetchAll();
+                                foreach($personal as $row){
+                                    echo '<label for="">Personal</label>
+                                          <select name="lista-personal" id="">
+                                            <option value="-1" >Seleccione Personal</option>';
+                                    echo '  <option value='.$row["peronal_id"].' >'.$row["personal_nombre"].' '.$row["personal_apaterno"].'</option>
+                                          </select>';
+                                }
+                            }else{
+                                echo '<p class="has-text-centered" >No hay personal registrado</p>';
+                            }
+                            $personal=null;
+                        ?>
+                </div>
+                
+            </form>
+        </div>
+        <div class="column">
+        <h2 class="has-text-centered title" >Agrege herramientas o refaciones</h2>
+            <?php
+                if(!isset($_GET['page'])){
+                    $pagina=1;
+                }else{
+                    $pagina=(int) $_GET['page'];
+                    if($pagina<=1){
+                        $pagina=1;
+                    }
+                }
+
+                $pagina=limpiar_cadena($pagina);
+                $url="index.php?vista=product_list&page="; /* <== */
+                $registros=15;
+                $busqueda="";
+
+                $categoria_id = (isset($_GET['category_id'])) ? $_GET['category_id'] : 0;
+
+                # Paginador producto #
+                require_once "./php/producto_salida.php";
+
+            ?>
+        </div>
+    </div>
 </div>
